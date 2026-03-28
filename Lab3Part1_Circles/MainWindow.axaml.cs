da09ca2d89ee1a843cceaee161d34a5b9ab6ac52
kio77to-del <kio77to@gmail.com>
2026-03-28 12:10:59 +0500

    добавил класс круга и контейнер кругов

---
+++ Lab3Part1_Circles/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Lab3Part1_Circles;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Lab3Part2_MVC/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Lab3Part2_MVC;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
